a18fb98143b16bc6bd2f623bc3d412045084b84f
Henrique <hp_bonfim@hotmail.com>
2020-07-09 07:56:31 -0300

    crawler criado

---
+++ README.md
@@ -0,0 +1,85 @@
+<h1 align="center">Crawler do Mercado Livre</h1>
+
+<p align="center">
+<a href="https://insomnia.rest/run/?label=&uri=https://github.com/hpbonfim/Crawler-Mercado-Livre/collection.json" target="_blank">
+<img src="https://insomnia.rest/images/run.svg" alt="Run in Insomnia">
+</a>
+
+<p align="center">
+<a href="https://github.com/hpbonfim/Crawler-Mercado-Livre#readme">
+<img alt="GitHub package.json dependency version (dev dep on branch)" src="https://img.shields.io/github/package-json/dependency-version/hpbonfim/Crawler-Mercado-Livre/dev/jest">
+</a>
+
+<a href="https://github.com/hpbonfim/Crawler-Mercado-Livre#readme">
+<img alt="GitHub package.json dependency version (prod)" src="https://img.shields.io/github/package-json/dependency-version/hpbonfim/Crawler-Mercado-Livre/crawler">
+</a>
+
+<a href="https://github.com/hpbonfim/Crawler-Mercado-Livre#readme">
+<img alt="GitHub package.json dependency version (prod)" src="https://img.shields.io/github/package-json/dependency-version/hpbonfim/Crawler-Mercado-Livre/express">
+</a>
+
+<a href="https://github.com/hpbonfim/Crawler-Mercado-Livre#readme">
+<img alt="size" src="https://img.shields.io/github/repo-size/hpbonfim/Crawler-Mercado-Livre"/>
+</a>
+</p>
+
+<p align="center">
+<a href="https://github.com/hpbonfim/Crawler-Mercado-Livre/commits/master">
+<img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/hpbonfim/Crawler-Mercado-Livre">
+</a>
+
+<a href="https://github.com/hpbonfim/Crawler-Mercado-Livre#readme">
+<img alt="Documentation" src="https://img.shields.io/badge/documentation-yes-brightgreen.svg" target="https://github.com/hpbonfim/Crawler-Mercado-Livre#readme" />
+</a>
+
+<a href="https://github.com/hpbonfim/Crawler-Mercado-Livre/graphs/commit-activity">
+<img alt="Maintenance" src="https://img.shields.io/badge/Maintained%3F-yes-green.svg" target="https://github.com/hpbonfim/Crawler-Mercado-Livre#readme" />
+</a>
+</p>
+
+</p>
+
+### Endpoints API
+
+
+- **POST**: `"/"` - **body: { search: string, limit: number }**   
+-- Realiza a consulta no site, trazendo o resultado via JSON [Mercado Livre](https://lista.mercadolivre.com.br/${search}) ( Type: x-www-form-urlencoded )
+
+#### Retorno Esperado
+```
+[
+    {
+        "name": String, // Nome do produto
+        "link": String,// Link do produto
+        "price": Number, // Preço
+        "store": String, // Nome da loja, se houver (dafault: null)
+        "state": String // Estado, se houver (dafault: null)
+    }
+]
+```
+
+
+### Para iniciar o Backend
+
+``` 
+$ npm install
+$ npm start
+```
+
+### Para testar o Backend
+
+``` 
+$ npm install
+$ npm test
+```
+
+
+
+|Tecnologias utilizadas  |
+|---------|
+|[Node v12.16.3](https://nodejs.org/en/)     |
+|[npm  6.14.4](https://www.npmjs.com/)    |
+|[Cors](https://www.npmjs.com/package/cors)     |
+|[Crawler](https://www.npmjs.com/package/crawler)   |
+|[Express](https://www.npmjs.com/package/express)   |
+
--- backend/core/server/controller/crawler.js
+++ backend/core/server/controller/crawler.js
@@ -1,7 +1,83 @@
 const Crawler = require("crawler")
 const cheerio = new Crawler()
 
-exports.crawlerService = (req, res, next) => {
+exports.crawlerService = (req, res) => {
 
-   
+    const limit = Number(req.body.limit)
+    if (isNaN(limit))
+        return res.status(404).json({ error: 'Digite um número válido no campo Limite!' })
+
+    const search = req.body.search
+    if (typeof search !== "string")
+        return res.status(404).json({ error: 'Digite uma palavra-chave no campo Procurar!' })
+
+
+    if (search === null || search === undefined || search === "")
+        return res.status(404).json({ error: 'Digite um valor válido para pesquisa' })
+
+    if (Number(limit) <= 0)
+        return res.status(404).json({ error: 'Digite um valor maior que 0 para retornar o resultado' })
+    else {
+        const url = `https://lista.mercadolivre.com.br/${search}`
+        console.log(`${new Date().toLocaleString('pt-BR')} | [crawler service] Making request to ${url}...`)
+
+        cheerio.queue([{
+            uri: url,
+            jQuery: true,
+            callback: (error, response, done) => {
+                if (error)
+                    return res.status(500).json({ error: error })
+                else {
+                    const items = []
+                    console.log(`${new Date().toLocaleString('pt-BR')} | [crawler service] Grabbed ${response.body.length} bytes from ${url}`)
+                    response.$('.results-item').slice(0, limit).each((i, elem) => {
+
+                        // tratamento de names
+                        const nameTitle = response.$(elem).find('span.main-title').text().trim() // quando search for genérico
+                        const nameSubtitle = response.$(elem).find('.item_subtitle').text().trim() // quando search = apartamento
+                        const setName = nameTitle !== "" ? nameTitle : nameSubtitle
+
+                        // tratamento do link
+                        const setLink = response.$(elem).find('a').attr("href")
+
+                        // tratamento de prices
+                        const price = Number(response.$(elem).find('.item__price > .price__decimals').text().trim())
+                        const chooseValue = price === 0 ? "" : `,${price}`
+                        const setValue = response.$(elem).find('.item__price > .price__symbol').text().trim() +
+                            response.$(elem).find('.item__price > .price__fraction').text().trim() + chooseValue
+
+                        // tratamento do stores
+                        const store = response.$(elem).find('.item__brand').text().trim()
+                        const setStore = store === "" ? null : `${store}`
+
+                        // tratamento dos states
+                        const stateTitle = response.$(elem).find('.item__location').text().trim()
+                        const stateSubtitle = response.$(elem).find('.item__title').text().trim()
+                        let setState = ""
+                        setState = nameTitle !== "" ? stateTitle : stateSubtitle
+                        setState = setState === "" ? null : setState
+
+                        const obj = {
+                            id: i,
+                            name: setName,
+                            link: setLink,
+                            price: setValue,
+                            store: setStore,
+                            state: setState
+                        }
+
+                        items.push(obj)
+                    })
+                    if (items.length == 0)
+                        return res.status(404).json({ error: 'Não há anúncios que coincidam com a sua busca.' })
+                    else {
+                        return (
+                            res.status(200).json(items),
+                            done()
+                        )
+                    }
+                }
+            }
+        }])
+    }
 }
